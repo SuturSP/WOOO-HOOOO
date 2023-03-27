c637affd54c5011da3d712ae86462435db12bd46
79137 <suturinsergey457@gmail.com>
2023-03-27 23:29:11 +0700

    Добавил 2 задание. Проверял в файле myfile.

--- reader3.py
+++ reader3.py
@@ -4,4 +4,4 @@ with open('stage3_test.csv', 'r', encoding="utf-8") as f:
         reader = csv.reader(f)
         writer = csv.writer(m)
         for item in reader:
-            writer.writerow([item[0],item[2],item[4]])
+            writer.writerow([item[0],item[2],item[4]]
