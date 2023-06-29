115e21720e18a167b4f44a7f7c87cc814a20c3f0
79137 <suturinsergey457@gmail.com>
2023-06-29 15:29:00 +0700

    Тесты.

--- wordcounter.py
+++ wordcounter.py
@@ -4,15 +4,10 @@ a = 0
 b = 0
 tree = etree.parse('annot.opcorpora.no_ambig.xml')
 root = tree.getroot()
-for text in root.findall('text'):
-    paragraphs = text.find("paragraphs")
-    for paragraph in paragraphs.findall("paragraph"):
-        for sentence in paragraph.findall("sentence"):
-            tokens = sentence.find("tokens")
-            for token in tokens.findall("token"):
-                if token.get("text") == "может":
-                    if token[0][0][0][0].get("v") =="CONJ":
-                        a += 1
-                    if token[0][0][0][0].get("v") =="VERB":
-                        b += 1
-print (a,b)
+for token in root.iter("token"):
+    if token.get("text") == "может" or token.get("text") == "Может":
+        if token[0][0][0][0].get("v") == "CONJ":
+            a += 1
+        if token[0][0][0][0].get("v") == "VERB":
+            b += 1
+print(a, b)
