47451e46634c86242b4b06503fa9bc3c6e9012ba
79137 <suturinsergey457@gmail.com>
2023-06-29 14:51:35 +0700

    Тесты.

--- reader.py
+++ reader.py
@@ -2,8 +2,7 @@ import csv
 with open('stage3_test.csv', 'r', encoding="utf-8") as f:
     with open('myfile.csv', 'w', encoding="utf-8") as m:
         reader = csv.DictReader(f)
-        fieldnames = ["Id", "Images", "Title", "Description", "Price"]
-        writer = csv.DictWriter(m, fieldnames = fieldnames)
+        writer = csv.DictWriter(m, fieldnames=reader.fieldnames)
         writer.writeheader()
         for item in reader:
             tmp = item['Images'].split(',')
